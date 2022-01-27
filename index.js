8515876e48fe25808f69cc335056fc85386b2571
RedonX53 <mian.muhammad.ahmad.53@gmail.com>
2022-01-27 14:20:06 +0500

    removed some unused files

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-// function requestEndPoint(endPointURL,loopController,imageKey,titleKey,insertId,htmlChef) {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open("Get", endPointURL);
-//   xhr.onload = function () {
-//     const dataX = JSON.parse(xhr.responseText);
-//     var key = Object.keys(dataX)[0];
-//     var objectList = dataX[key];
-//     getListData(objectList,loopController,imageKey,titleKey,insertId,htmlChef);
-//   };
-//   xhr.send();
-// }
+function requestEndPoint(endPointURL,loopController,imageKey,titleKey,insertId,htmlChef) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("Get", endPointURL);
+  xhr.onload = function () {
+    const dataX = JSON.parse(xhr.responseText);
+    var key = Object.keys(dataX)[0];
+    var objectList = dataX[key];
+    getListData(objectList,loopController,imageKey,titleKey,insertId,htmlChef);
+  };
+  xhr.send();
+}
 
-// function getListData(data,loopController,imageKey,titleKey,insertId,htmlChef) {
-//   console.log(imageKey, titleKey);
-//   for (let i = 0; i < loopController; i++) {
-//     var title = data[i][titleKey];
-//     var img = data[i][imageKey];
-//     document.getElementById(insertId).innerHTML += htmlChef(title, img);
-//   }
-// }
-// requestEndPoint("json_data/latest_meals.json",8,"imgPath","mealName","latest-meal-item",generateHtml);
-// requestEndPoint("popularMeal.json",4,"strCategoryThumb","strCategory","popular-row",generateHtml);
-// requestEndPoint("randomMeal.json",8,"strMealThumb","strMeal","random-row",generateHtml);
-// requestEndPoint("randomingredients.json",4,"strCategoryThumb","strCategory","ingredient-row",generateHtml);
+function getListData(data,loopController,imageKey,titleKey,insertId,htmlChef) {
+  console.log(imageKey, titleKey);
+  for (let i = 0; i < loopController; i++) {
+    var title = data[i][titleKey];
+    var img = data[i][imageKey];
+    document.getElementById(insertId).innerHTML += htmlChef(title, img);
+  }
+}
+requestEndPoint("json_data/latest_meals.json",8,"imgPath","mealName","latest-meal-item",generateHtml);
+requestEndPoint("json_data/latest_meals.json",4,"strCategoryThumb","strCategory","popular-row",generateHtml);
+requestEndPoint("json_data/latest_meals.json",8,"strMealThumb","strMeal","random-row",generateHtml);
+requestEndPoint("json_data/latest_meals.json",4,"strCategoryThumb","strCategory","ingredient-row",generateHtml);
 
-// function generateHtml(title, img) {
-//   return  `<div class="random-item">
-//   <img src="${img}" />
-//   <h3>${title}</h3>
-// </div>`;
-// }
+function generateHtml(title, img) {
+  return  `<div class="random-item">
+  <img src="${img}" />
+  <h3>${title}</h3>
+</div>`;
+}
 
